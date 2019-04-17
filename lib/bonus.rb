4559033fff427897c55d3293daf43f252670102b
Christian Pinto <pintochristian2@gmail.com>
2019-04-17 01:15:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  :hero.object_id = "dead"
+  :hero.object_id["status"] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
