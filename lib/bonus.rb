7b3501cb26a79b3ffeba61552d59421c45b99123
Christian Pinto <pintochristian2@gmail.com>
2019-04-17 01:24:45 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -4,7 +4,7 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", status: "dead"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
@@ -22,8 +22,8 @@ def bonus
   }
 
   #code your solution here:
-  binding.pry
-  :hero["status"] => "dead"
+  epic_tragedy
+  
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
